ea6790fa3eea08200b8de5d5f3ba3443e2d77680
RavenDigitalStudio <90115718+RavenDigitalStudio@users.noreply.github.com>
2021-10-07 06:12:03 -0600

    Add files via upload

--- RavenWebsite/js.js
+++ RavenWebsite/js.js
@@ -30,14 +30,14 @@ faders.forEach(fader => {
 })
 
 /* allow for smooth slow scrolling */
-$(".nav a").on("click", function (e) {
-  // 1
-  e.preventDefault();
-  // 2
-  const href = $(this).attr("href");
-  // 3
-  $("html, body").animate({ scrollTop: $(href).offset().top }, 1000);
-});
+// $(".nav a").on("click", function (e) {
+//   // 1
+//   e.preventDefault();
+//   // 2
+//   const href = $(this).attr("href");
+//   // 3
+//   $("html, body").animate({ scrollTop: $(href).offset().top }, 1000);
+// });
 
 
 });
