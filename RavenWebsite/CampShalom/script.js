c6a50380a35a79905d844fb83e3e66781086e22d
RavenDigitalStudio <90115718+RavenDigitalStudio@users.noreply.github.com>
2021-11-21 00:48:20 -0700

    Add files via upload

--- RavenWebsite/CampShalom/script.js
+++ RavenWebsite/CampShalom/script.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+const menu = document.querySelector(".menu");
+//console.log(menu.children);
+console.log(window.location.pathname.split("/")[1]);
+
+switch (window.location.pathname.split("/")[1]) {
+  case "about.html": {
+    menu.children[0].children[0].style.color = "#F7ECA8";
+    break;
+  }
+  case "campDetails.html": {
+    menu.children[1].children[0].style.color = "#F7ECA8";
+    break;
+  }
+  case "registration.html": {
+    menu.children[2].children[0].style.color = "#F7ECA8";
+    break;
+  }
+  case "counselorApplication.html": {
+    menu.children[3].children[0].style.color = "#F7ECA8";
+    break;
+  }
+  case "scholarshipApplication.html": {
+    menu.children[4].children[0].style.color = "#F7ECA8";
+    break;
+  }
+  case "contact.html": {
+    menu.children[5].children[0].style.color = "#F7ECA8";
+    break;
+  }
+}
